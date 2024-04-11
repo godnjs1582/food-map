0c54db6c8258c51100bacc25c86746d5a74e7272
Kwon HaeWon <qpinky12@naver.com>
2024-04-12 02:20:19 +0900

    FEAT:geolocation을 통하여 현재 내 위치 가져오기

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {Header} from '../components/Header/Header';
 import {Icon} from '../components/Icons';
+import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 const MainScreen = () => {
+  const [currentRegion, setCurrentRegion] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({
+    latitude: 37.560214,
+    longitude: 126.9775521,
+  });
+
+  const getMyLocation = useCallback(() => {
+    Geolocation.getCurrentPosition(position => {
+      setCurrentRegion({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    getMyLocation();
+  }, [getMyLocation]);
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Header>
         <Header.Title title="Main" />
       </Header>
-      <Icon name="close" size={24} color="black" />
+      <MapView
+        style={{flex: 1}}
+        region={{
+          latitude: currentRegion.latitude,
+          longitude: currentRegion.longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
+      <Marker
+        coordinate={{
+          latitude: currentRegion.latitude,
+          longitude: currentRegion.longitude,
+        }}
+      />
     </View>
   );
 };
